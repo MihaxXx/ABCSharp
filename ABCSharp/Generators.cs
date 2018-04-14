47f670b3339366a1e31f81a3dd5168132a8a7d8e
Danil Kabanov <dkabanov96@gmail.com>
2018-04-14 17:51:21 +0300

    Simple range gens

---
+++ ABCSharp/Generators.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ABCSharp
+{
+    public static class Generators
+    {
+        /// <summary>
+        /// Returns sequence of integers from begin to end (excluded)
+        /// </summary>
+        public static IEnumerable<int> Range(int begin, int end)
+        {
+            var delta = begin < end ? 1 : -1;
+            for (var i = begin; i != end; i += delta)
+                yield return i;
+        }
+
+        /// <summary>
+        /// Returns sequence of integers from zero to end (excluded)
+        /// </summary>
+        public static IEnumerable<int> Range(int end)
+        {
+            for (var i = 0; i != end; ++i)
+                yield return i;
+        }
+    }
+}
